8759e647bb71f5f5528ddc1b99df3fba3cd6261e
Ugiomoh Daniel <ugiomohd@yahoo.com>
2022-11-07 08:42:37 +0100

    done-project

--- src/Components/InnerRepo.jsx
+++ src/Components/InnerRepo.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import axios from "axios";
 import { useState, useEffect } from "react";
 
@@ -13,7 +13,7 @@ const InnerRepo = () => {
         "https://api.github.com/users/dannyswagg/repos"
       );
       console.log(response.data);
-      const result = response.data.find((yourdata) => yourdata.id === id);
+      const result = response.data.find((yourData) => yourData.id === id);
       if (result) {
         setResult(result);
       }
@@ -21,40 +21,17 @@ const InnerRepo = () => {
     getRepo();
   });
 
-  //   useEffect(() => {
-  //     const getRepos = () => {
-  //       axios
-  //         .get("https://api.github.com/users/dannyswagg/repos")
-  //         .then((response) => {
-  //           console.log(response);
-  //           const result = response.data.find((yourData) => yourData.id === id);
-  //           if (result) {
-  //             setResult(result);
-  //           }
-  //           console.log(result);
-  //           // setRepo(myRepo) || setLoading(false);
-  //         });
-  //     };
-  //     getRepos();
-  //   }, []);
-
   return (
     <>
-      <div className="pt-8 bg-[#F6EED8] pb-8 p-4">
-        <div className="text-center mx-auto lg:w-2/4 md:w-1/3 sm:w-[18rem]">
-          <div
-            className="rounded-sm shadow-lg p-5 bg-black text-[#C19167]"
-            key={id}
-          >
-            <div className="card-body">
-              <h1>Name: {result.name}</h1>
-              <h2>Full-Name: {result.full_name}</h2>
-              <h3>
-                <h1>Size: {result.size}kb</h1>
-              </h3>
-            </div>
-          </div>
-        </div>
+      <div className="bg-black text-[#c19167] lg:w-2/4 md:w-fit w-fit mx-auto mt-8 rounded p-8">
+        <h1 className="p-8 text-center font-bold">
+          Repo id: <br /> {id} <br />
+          <Link to="/">
+            <button className="rounded bg-[#c19167] text-black p-3 mt-3 font-bold">
+              Go Home
+            </button>
+          </Link>
+        </h1>
       </div>
     </>
   );
